4a1c40ffc8eb9c9ea1bf6ee072b73f0922164651
yechayo <2628505364@qq.com>
2025-03-14 13:20:46 +0800

    云超市

--- main.jsx
+++ main.jsx
@@ -8,9 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    
       <App />
-      
     </BrowserRouter>
   </StrictMode>,
 )
